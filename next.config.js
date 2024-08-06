766a5d5b0e1f7f625f63953260e4a1c5069db8d4
Programmovic <adamforbusiness@outlook.com>
2024-08-06 16:58:48 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
-const withNextIntl = require('next-intl/plugin')();
+const withNextIntl = require("next-intl/plugin")();
 const nextConfig = withNextIntl({
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
   images: {
-    domains: ['*'], // Allow images from any hostname
+    domains: ["*"], // Allow images from any hostname
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 });
 

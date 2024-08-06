ffcf6634e2c628765258638f28dd66f4e0495983
Programmovic <adamforbusiness@outlook.com>
2024-08-06 12:21:37 +0300

    update layout.jsx

--- src/app/[locale]/(DashboardLayout)/layout.jsx
+++ src/app/[locale]/(DashboardLayout)/layout.jsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 import { styled, Container, Box } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Header from "@/app/(DashboardLayout)/layout/header/Header";
 import Sidebar from "@/app/(DashboardLayout)/layout/sidebar/Sidebar";
 import { usePathname } from "next/navigation";
@@ -11,32 +11,43 @@ const MainWrapper = styled("div")(() => ({
   width: "100%",
 }));
 
-const PageWrapper = styled("div")(() => ({
+const PageWrapper = styled("div")(({ sidebarWidth }) => ({
   display: "flex",
   flexGrow: 1,
   paddingBottom: "60px",
   flexDirection: "column",
   zIndex: 1,
   backgroundColor: "transparent",
+  maxWidth: `calc(100% - ${sidebarWidth})`, // Adjust maxWidth to account for the sidebar width
+  transition: 'max-width 0.3s', // Optional: smooth transition
 }));
 
 export default function RootLayout({ children, params: { locale } }) {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
   const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
+  const [sidebarWidth, setSidebarWidth] = useState('0px'); // Default width
+
   const pathname = usePathname(); // Get the current pathname
 
   // Check if the current path is the home page
   const isHomePage = pathname === `/${locale}`; // Adjust the condition as needed
-  console.log(isHomePage, pathname)
+  console.log(isHomePage, pathname);
+
+  useEffect(() => {
+    if (sidebarRef.current) {
+      setSidebarWidth(`270px`);
+    }
+  }, [sidebarRef.current, isSidebarOpen]);
+
+  const handleToggleSidebar = (newSidebarState) => {
+    setSidebarOpen(newSidebarState);
+  };
 
   // Render only the children if it's the home page
   if (isHomePage) {
     return <>{children}</>;
   }
-
-  const handleToggleSidebar = (newSidebarState) => {
-    setSidebarOpen(newSidebarState);
-  };
 
   return (
     <MainWrapper className="mainwrapper">
@@ -44,6 +55,7 @@ export default function RootLayout({ children, params: { locale } }) {
       {/* Sidebar */}
       {/* ------------------------------------------- */}
       <Sidebar
+        ref={sidebarRef}
         isSidebarOpen={isSidebarOpen}
         isMobileSidebarOpen={isMobileSidebarOpen}
         onSidebarClose={() => setMobileSidebarOpen(false)}
@@ -53,7 +65,7 @@ export default function RootLayout({ children, params: { locale } }) {
       {/* ------------------------------------------- */}
       {/* Main Wrapper */}
       {/* ------------------------------------------- */}
-      <PageWrapper className="page-wrapper">
+      <PageWrapper className="page-wrapper" sidebarWidth={sidebarWidth}>
         {/* ------------------------------------------- */}
         {/* Header */}
         {/* ------------------------------------------- */}

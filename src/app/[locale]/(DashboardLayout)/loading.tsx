99905c51444d5bfb38335584176ad91f88e9af84
Programmovic <adamforbusiness@outlook.com>
2024-09-07 15:38:09 +0300

    update page.jsx and loading.tsx

--- src/app/[locale]/(DashboardLayout)/loading.tsx
+++ src/app/[locale]/(DashboardLayout)/loading.tsx
@@ -1,42 +1,48 @@
-'use client';
+'use client'
 import React from "react";
 import { CircularProgress, Box } from "@mui/material";
-import '../loading.scss'; // Ensure this file is correctly imported
+import '../loading.scss'; // Make sure to style this properly in your CSS file
 
 const Loading = () => {
   return (
     <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
       sx={{
-        width: "100%",
-        height: "100vh", // Ensure it covers the full viewport height
-        overflow: 'hidden', // To contain the animation
-        position: 'relative', // Positioning for the road and car images
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent background
+        zIndex: 1300, // To make sure it's on top of other content
       }}
     >
       <Box
-        className="road"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        p={3}
         sx={{
-          position: 'absolute',
-          bottom: '20px', // Adjust the distance from the bottom as needed
-          width: '100%',
-          height: '100px', // Height of the road
-          zIndex: 1, // Ensure it's below the car
+          backgroundColor: 'white',
+          borderRadius: '16px',
+          boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)', // Subtle shadow for a floating effect
+          width: "50%"
+
         }}
-      />
-      <Box
-        component="img"
-        src="/images/logos/asm_logo1.png"
-        alt="Logo"
-        width={500} // Adjust the size as needed
-        height={400} // Adjust the size as needed
-        className="car-image" // Apply animation class
-        sx={{ position: 'relative', zIndex: 2 }} // Ensure the car is above the road
-      />
-      <CircularProgress sx={{ mt: 2, color: '#1976d2' }} /> {/* Color to match theme */}
+      >
+        <Box
+          component="img"
+          src="/images/logos/asm_logo.png"
+          alt="Logo"
+          width={500} // Adjust the size as needed
+          height={250} // Adjust the size as needed
+          className="pulse"
+        />
+        <CircularProgress sx={{ mt: 2, color: '#1976d2' }} /> {/* Color to match theme */}
+      </Box>
     </Box>
   );
 };
